09f8d38aac7bf0a0dd90830b507c2139ac3651b1
doankhiem <khiembi161219@gmail.com>
2024-03-24 21:28:53 +0700

    clear old project

--- resources/views/user/layouts/footer/footer-bottom.blade.php
+++
@@ -1,8 +0,0 @@
-<div class="footer-bottom pb-30">
-    <div class="container">
-        <div class="copyright-text text-center">                    
-            <p>Copyright © 2018 <a target="_blank" href="#">Truemart</a> All Rights Reserved.</p>
-        </div>
-    </div>
-    <!-- Container End -->
-</div>
--- resources/views/user/product-detail.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('user.layouts.master')
-
-@section('main')
-
-
-
-@endsection
