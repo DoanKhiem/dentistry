318d65c34ab95d93c99a5451d73990df9dccbc07
khiemdev <khiembi161219@gmail.com>
2024-03-28 10:48:11 +0700

    make controller resource and route sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -9,22 +9,22 @@
             <nav>
                 <ul class="metismenu" id="menu">
                     <li class="active">
-                        <a href="javascript:void(0)" aria-expanded="true"><i class="ti-dashboard"></i><span>Bảng điều khiển</span></a>
+                        <a href="{{route('dashboard')}}" aria-expanded="true"><i class="ti-dashboard"></i><span>Bảng điều khiển</span></a>
                     </li>
                     <li class="">
-                        <a href="javascript:void(0)" aria-expanded="true"><i class="ti-dashboard"></i><span>Quản lý nhân viên</span></a>
+                        <a href="{{route('staff.index')}}" aria-expanded="true"><i class="ti-dashboard"></i><span>Quản lý nhân viên</span></a>
                     </li>
                     <li class="">
-                        <a href="javascript:void(0)" aria-expanded="true"><i class="ti-dashboard"></i><span>Quản lý bác sĩ</span></a>
+                        <a href="{{route('doctor.index')}}" aria-expanded="true"><i class="ti-dashboard"></i><span>Quản lý bác sĩ</span></a>
                     </li>
                     <li class="">
-                        <a href="javascript:void(0)" aria-expanded="true"><i class="ti-dashboard"></i><span>Quản lý bệnh nhân</span></a>
+                        <a href="{{route('patient.index')}}" aria-expanded="true"><i class="ti-dashboard"></i><span>Quản lý bệnh nhân</span></a>
                     </li>
                     <li class="">
-                        <a href="javascript:void(0)" aria-expanded="true"><i class="ti-dashboard"></i><span>Quản lý cuộc hẹn</span></a>
+                        <a href="{{route('appointment.index')}}" aria-expanded="true"><i class="ti-dashboard"></i><span>Quản lý cuộc hẹn</span></a>
                     </li>
                     <li class="">
-                        <a href="javascript:void(0)" aria-expanded="true"><i class="ti-dashboard"></i><span>Dịch vụ</span></a>
+                        <a href="{{route('service.index')}}" aria-expanded="true"><i class="ti-dashboard"></i><span>Dịch vụ</span></a>
                     </li>
                     <li class="">
                         <a href="javascript:void(0)" aria-expanded="true"><i class="ti-dashboard"></i><span>Báo cáo, thống kê</span></a>
