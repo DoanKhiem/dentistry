674dcf7e088635ccd6a7ed977dc578502284424d
khiemdev <khiembi161219@gmail.com>
2024-03-28 09:55:18 +0700

    log out

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -42,30 +42,7 @@
     <!-- main content area start -->
     <div class="main-content">
         <!-- page title area start -->
-        <div class="page-title-area">
-            <div class="row align-items-center">
-                <div class="col-sm-6">
-                    <div class="breadcrumbs-area clearfix">
-                        <h4 class="page-title pull-left">Dashboard</h4>
-                        <ul class="breadcrumbs pull-left">
-                            <li><a href="index.html">Home</a></li>
-                            <li><span>Dashboard</span></li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="col-sm-6 clearfix">
-                    <div class="user-profile pull-right">
-                        <img class="avatar user-thumb" src="{{asset('assets/images/author/avatar.png')}}" alt="avatar">
-                        <h4 class="user-name dropdown-toggle" data-toggle="dropdown">Kumkum Rai <i class="fa fa-angle-down"></i></h4>
-                        <div class="dropdown-menu">
-                            <a class="dropdown-item" href="#">Message</a>
-                            <a class="dropdown-item" href="#">Settings</a>
-                            <a class="dropdown-item" href="#">Log Out</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+        @include('layouts.title-area')
         <!-- page title area end -->
         <div class="main-content-inner">
             <!-- sales report area start -->
