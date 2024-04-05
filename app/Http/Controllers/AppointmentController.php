8b1fbe9399dc984c8870db5d4fd890f0b49a3196
khiemdev <khiembi161219@gmail.com>
2024-04-05 13:47:25 +0700

    admin edit booking

--- app/Http/Controllers/AppointmentController.php
+++ app/Http/Controllers/AppointmentController.php
@@ -66,7 +66,11 @@ class AppointmentController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        $patients = Patient::orderBy('created_at', 'desc')->get();
+        $doctors = Doctor::orderBy('created_at', 'desc')->get();
+        $services = Service::orderBy('created_at', 'desc')->get();
+        $item = Appointment::findOrFail($id);
+        return view('appointment.edit', compact('item', 'patients', 'doctors', 'services'));
     }
 
     /**
@@ -74,7 +78,24 @@ class AppointmentController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $item = Doctor::findOrFail($id);
+        $this->validate($request, [
+            'code' => 'required|unique:appointments,code,' . $id,
+            'patient_id' => 'required',
+            'doctor_id' => 'required',
+            'service_id' => 'required',
+            'time' => 'required',
+            'status' => 'required',
+        ]);
+        $service = Service::findOrFail($request->service_id);
+        $request->merge(['price' => $service->price]);
+        $data = $request->all();
+        $status = $item->update($data);
+        if ($status) {
+            return redirect()->route('appointment.index')->with('success', 'Sửa cuộc hẹn thành công');
+        } else {
+            return back()->with('error', 'Lỗi sửa cuộc hẹn');
+        }
     }
 
     /**
